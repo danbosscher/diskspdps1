f6d4597cfcd09ccb53986575f3ff23cfd68364d0
Dan Bosscher <dabossch@microsoft.com>
2023-07-28 12:40:34 +0100

    fix path

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://aka.ms/getdiskspd","C:\temp\diskspd.zip")
 Expand-Archive -LiteralPath C:\temp\diskspd.zip -DestinationPath C:\diskspd
-C:\diskspd\diskspd.exe -t2 -o32 -b4k -r4k -w0 -d120 -Sh -D -L -c5G
+C:\diskspd\amd64\diskspd.exe -t2 -o32 -b4k -r4k -w0 -d120 -Sh -D -L -c5G
