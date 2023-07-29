670d02287557f5ece80d671c6eff79e5f2d31483
Dan Bosscher <dabossch@microsoft.com>
2023-07-29 14:05:27 +0100

    tweaks

---
+++ createjob.sh
@@ -0,0 +1,4 @@
+az batch job create \
+  --account-name dabosschstoragerepro \
+  --account-endpoint dabosschstoragerepro.westeurope.batch.azure.com \
+  --template diskspd-job.json
--- createpool.sh
+++ createpool.sh
@@ -1,5 +1,4 @@
-timestamp=$(date +%d-%m-%Y_%H-%M-%S)
-az batch job create \
+az batch pool create \
   --account-name dabosschstoragerepro \
   --account-endpoint dabosschstoragerepro.westeurope.batch.azure.com \
-  --template diskspd-job.json
+  --template diskspd-pool.json
