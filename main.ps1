6e9adc0369f59d8f06e72b54456b03f7afb04651
Dan Bosscher <dabossch@microsoft.com>
2023-07-29 10:59:16 +0100

    variables

--- main.ps1
+++ main.ps1
@@ -2,16 +2,12 @@
 ## https://github.com/danbosscher/diskspdps1
 
 # Variables
-$url="https://aka.ms/getdiskspd"
-$temp="C:\temp"
-$diskspddir="C:\diskspd"
-
 # Grab diskspd if not already installed
-if (-not(Test-Path -Path $diskspddir)) {
+if (-not(Test-Path -Path C:\diskspd)) {
     try {
         $client = new-object System.Net.WebClient
-        $client.DownloadFile("https://aka.ms/getdiskspd","$temp\diskspd.zip")
-        Expand-Archive -LiteralPath $temp\diskspd.zip -DestinationPath $diskspddir
+        $client.DownloadFile("https://aka.ms/getdiskspd","C:\temp\diskspd.zip")
+        Expand-Archive -LiteralPath C:\temp\diskspd.zip -DestinationPath C:\diskspd
     }
     catch {
         Write-Output "Error downloading or extracting diskspd archive: $_"
@@ -20,9 +16,9 @@ if (-not(Test-Path -Path $diskspddir)) {
 }
 
 # Delete old test file if present
-if (Test-Path -Path $temp\test.dat) {
+if (Test-Path -Path C:\temp\test.dat) {
     try {
-        Remove-Item -Path $temp\test.dat
+        Remove-Item -Path C:\temp\test.dat
     }
     catch {
         Write-Output "Error removing old test file: $_"
@@ -35,4 +31,4 @@ else {
 }
 
 # Run diskspd
-C:\diskspd\amd64\diskspd.exe -d10 -W15 -C15 -L -r -w40 -t8 -b64K -Su -c10G $temp\test.dat
+C:\diskspd\amd64\diskspd.exe -d10 -W15 -C15 -L -r -w40 -t8 -b64K -Su -c10G C:\temp\test.dat
