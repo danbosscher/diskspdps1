88b47a69436049fa9756f8149126cfba9ab70322
Dan Bosscher <dabossch@microsoft.com>
2023-07-29 10:58:19 +0100

    timestamp

--- az batch task create.sh
+++ az batch task create.sh
@@ -1,6 +1,7 @@
+timestamp=$(date +%d-%m-%Y_%H-%M-%S)
 az batch task create \
   --account-name dabosschstoragerepro \
   --account-endpoint dabosschstoragerepro.westeurope.batch.azure.com \
-  --task-id 1 \
+  --task-id $timestamp \
   --job-id DiskSpdJob \
   --command-line "powershell -command (iex ((new-object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/danbosscher/diskspdps1/main/main.ps1')))" \
