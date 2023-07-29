cfed95543417ca780e788314a9602845a95aa85b
Dan Bosscher <dabossch@microsoft.com>
2023-07-29 14:06:48 +0100

    go

--- createjob.sh
+++ createjob.sh
@@ -1,4 +1,4 @@
 az batch job create \
   --account-name dabosschstoragerepro \
   --account-endpoint dabosschstoragerepro.westeurope.batch.azure.com \
-  --template diskspd-job.json
+  --template config/diskspd-job.json
--- createpool.sh
+++ createpool.sh
@@ -1,4 +1,4 @@
 az batch pool create \
   --account-name dabosschstoragerepro \
   --account-endpoint dabosschstoragerepro.westeurope.batch.azure.com \
-  --template diskspd-pool.json
+  --template config/diskspd-pool.json
